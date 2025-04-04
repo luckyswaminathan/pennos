2ebe459636f100e8a6adf19a7a2ff544ba1d7c7c
luckyswaminathan <lakshman.swaminathan1@gmail.com>
2025-04-04 15:29:34 -0400

    s nice

--- src/scheduler.c
+++ src/scheduler.c
@@ -122,4 +122,27 @@ int s_kill(pid_t pid) {
     return -1;
 }
 
-
+int s_nice(pid_t pid, int priority) {
+    pcb_t* proc = scheduler_state->processes.head;
+    while (proc != NULL) {
+        if (proc->pid == pid) {
+            LOG_INFO("Setting priority of process %d to %d", proc->pid, priority);
+            if (proc->priority != priority) {
+                linked_list_remove(&scheduler_state->priority_medium, proc);
+                if (priority == PRIORITY_HIGH) {
+                    linked_list_push_tail(&scheduler_state->priority_high, proc);
+                } else if (priority == PRIORITY_MEDIUM) {
+                    linked_list_push_tail(&scheduler_state->priority_medium, proc);
+                } else if (priority == PRIORITY_LOW) {
+                    linked_list_push_tail(&scheduler_state->priority_low, proc);
+                }
+            }
+            proc->priority = priority;
+            
+            return 0;
+        }
+        proc = proc->next;
+    }
+    return -1;
+}
+    
