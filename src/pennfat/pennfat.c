8683d7daf301f582a11bc63df8d4dcab7369c3ca
Your Name <you@seas.upenn.edu>
2025-04-10 16:01:28 -0400

    Changed k function calls to reflect correct parameters being passed through

--- src/pennfat/pennfat.c
+++ src/pennfat/pennfat.c
@@ -207,38 +207,38 @@ int main(void) {
 			}
 			
 			// Open the source file for reading
-			int src_fd = k_open(&fs, tokens[1], F_READ);
+			int src_fd = k_open(tokens[1], F_READ);
 			if (src_fd < 0) {
 				fprintf(stderr, "mv: failed to open source file with error code %d\n", src_fd);
 				goto cleanup_tokens;
 			}
 			
 			// Create the destination file
-			int dest_fd = k_open(&fs, tokens[2], F_WRITE);
+			int dest_fd = k_open(tokens[2], F_WRITE);
 			if (dest_fd < 0) {
 				fprintf(stderr, "mv: failed to open destination file with error code %d\n", dest_fd);
-				k_close(&fs, src_fd);
+				k_close(src_fd);
 				goto cleanup_tokens;
 			}
 			
 			// Copy the contents
 			char buffer[1024];
 			int bytes_read;
-			while ((bytes_read = k_read(&fs, src_fd, sizeof(buffer), buffer)) > 0) {
-				if (k_write(&fs, dest_fd, buffer, bytes_read) < 0) {
+			while ((bytes_read = k_read(src_fd, sizeof(buffer), buffer)) > 0) {
+				if (k_write(dest_fd, buffer, bytes_read) < 0) {
 					fprintf(stderr, "mv: failed to write to destination file\n");
-					k_close(&fs, src_fd);
-					k_close(&fs, dest_fd);
+					k_close(src_fd);
+					k_close(dest_fd);
 					goto cleanup_tokens;
 				}
 			}
 			
 			// Close both files
-			k_close(&fs, src_fd);
-			k_close(&fs, dest_fd);
+			k_close(src_fd);
+			k_close(dest_fd);
 			
 			// Delete the source file
-			if (k_unlink(&fs, tokens[1]) < 0) {
+			if (k_unlink(tokens[1]) < 0) {
 				fprintf(stderr, "mv: failed to remove source file\n");
 				goto cleanup_tokens;
 			}
@@ -255,7 +255,7 @@ int main(void) {
 			
 			// Remove the file
 			// TODO: Check w/ Aagam if unlink is complete in fat.c
-			int unlink_status = k_unlink(&fs, tokens[1]);
+			int unlink_status = k_unlink(tokens[1]);
 			if (unlink_status < 0) {
 				fprintf(stderr, "rm: failed to remove file with error code %d\n", unlink_status);
 				goto cleanup_tokens;
@@ -288,7 +288,7 @@ int main(void) {
 			// Case 1: cat -w FILE or cat -a FILE (read from terminal, write to file)
 			if ((write_mode || append_mode) && n_tokens == 3) {
 				int mode = write_mode ? F_WRITE : F_APPEND;
-				int fd = k_open(&fs, output_file, mode);
+				int fd = k_open(output_file, mode);
 				if (fd < 0) {
 					fprintf(stderr, "cat: failed to open output file with error code %d\n", fd);
 					goto cleanup_tokens;
@@ -299,15 +299,15 @@ int main(void) {
 				char buffer[1024];
 				int bytes_read;
 				
-				while ((bytes_read = k_read(&fs, STDIN_FD, sizeof(buffer), buffer)) > 0) {
-					if (k_write(&fs, fd, buffer, bytes_read) < 0) {
+				while ((bytes_read = read(STDIN_FD, buffer, sizeof(buffer))) > 0) {
+					if (k_write(fd, buffer, bytes_read) < 0) {
 						fprintf(stderr, "cat: failed to write to file\n");
-						k_close(&fs, fd);
+						k_close(fd);
 						goto cleanup_tokens;
 					}
 				}
 				
-				k_close(&fs, fd);
+				k_close(fd);
 			}
 			// Case 2: cat FILE1 FILE2 ... [-w|-a OUTPUT_FILE]
 			else if (n_tokens >= 2) {
@@ -315,7 +315,7 @@ int main(void) {
 				int out_fd = -1;
 				if (output_file != NULL) {
 					int mode = append_mode ? F_APPEND : F_WRITE;
-					out_fd = k_open(&fs, output_file, mode);
+					out_fd = k_open(output_file, mode);
 					if (out_fd < 0) {
 						fprintf(stderr, "cat: failed to open output file with error code %d\n", out_fd);
 						goto cleanup_tokens;
@@ -331,12 +331,12 @@ int main(void) {
 					}
 					
 					// Open current file
-					int in_fd = k_open(&fs, tokens[i], F_READ);
+					int in_fd = k_open(tokens[i], F_READ);
 					if (in_fd < 0) {
 						fprintf(stderr, "cat: failed to open input file %s with error code %d\n", 
 								tokens[i], in_fd);
 						if (out_fd >= 0) {
-							k_close(&fs, out_fd);
+							k_close(out_fd);
 						}
 						goto cleanup_tokens;
 					}
@@ -344,24 +344,24 @@ int main(void) {
 					// Read and write
 					char buffer[1024];
 					int bytes_read;
-					while ((bytes_read = k_read(&fs, in_fd, sizeof(buffer), buffer)) > 0) {
+					while ((bytes_read = k_read(in_fd, sizeof(buffer), buffer)) > 0) {
 						// Write to output file if specified, otherwise to stdout
 						int write_fd = (out_fd >= 0) ? out_fd : STDOUT_FD;
-						if (k_write(&fs, write_fd, buffer, bytes_read) < 0) {
+						if (k_write(write_fd, buffer, bytes_read) < 0) {
 							fprintf(stderr, "cat: write failed\n");
-							k_close(&fs, in_fd);
+							k_close(in_fd);
 							if (out_fd >= 0) {
-								k_close(&fs, out_fd);
+								k_close(out_fd);
 							}
 							goto cleanup_tokens;
 						}
 					}
 					
-					k_close(&fs, in_fd);
+					k_close(in_fd);
 				}
 				
 				if (out_fd >= 0) {
-					k_close(&fs, out_fd);
+					k_close(out_fd);
 				}
 			}
 			else {
@@ -386,7 +386,7 @@ int main(void) {
 				}
 				
 				// Create destination file in PennFAT
-				int pennfat_fd = k_open(&fs, tokens[3], F_WRITE);
+				int pennfat_fd = k_open(tokens[3], F_WRITE);
 				if (pennfat_fd < 0) {
 					fprintf(stderr, "cp: failed to create PennFAT file with error code %d\n", pennfat_fd);
 					close(host_fd);
@@ -397,21 +397,21 @@ int main(void) {
 				char buffer[1024];
 				ssize_t bytes_read;
 				while ((bytes_read = read(host_fd, buffer, sizeof(buffer))) > 0) {
-					if (k_write(&fs, pennfat_fd, buffer, bytes_read) < 0) {
+					if (k_write(pennfat_fd, buffer, bytes_read) < 0) {
 						fprintf(stderr, "cp: failed to write to PennFAT file\n");
 						close(host_fd);
-						k_close(&fs, pennfat_fd);
+						k_close(pennfat_fd);
 						goto cleanup_tokens;
 					}
 				}
 				
 				close(host_fd);
-				k_close(&fs, pennfat_fd);
+				k_close(pennfat_fd);
 			}
 			// cp PENNFAT_FILE -h HOST_FILE (from pennfat to host)
 			else if (n_tokens == 4 && strcmp(tokens[2], "-h") == 0) {
 				// Open PennFAT file
-				int pennfat_fd = k_open(&fs, tokens[1], F_READ);
+				int pennfat_fd = k_open(tokens[1], F_READ);
 				if (pennfat_fd < 0) {
 					fprintf(stderr, "cp: failed to open PennFAT file with error code %d\n", pennfat_fd);
 					goto cleanup_tokens;
@@ -421,56 +421,56 @@ int main(void) {
 				int host_fd = open(tokens[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 				if (host_fd < 0) {
 					fprintf(stderr, "cp: failed to create host file: %s\n", tokens[3]);
-					k_close(&fs, pennfat_fd);
+					k_close(pennfat_fd);
 					goto cleanup_tokens;
 				}
 				
 				// Copy data
 				char buffer[1024];
 				int bytes_read;
-				while ((bytes_read = k_read(&fs, pennfat_fd, sizeof(buffer), buffer)) > 0) {
+				while ((bytes_read = k_read(pennfat_fd, sizeof(buffer), buffer)) > 0) {
 					if (write(host_fd, buffer, bytes_read) < 0) {
 						fprintf(stderr, "cp: failed to write to host file\n");
-						k_close(&fs, pennfat_fd);
+						k_close(pennfat_fd);
 						close(host_fd);
 						goto cleanup_tokens;
 					}
 				}
 				
-				k_close(&fs, pennfat_fd);
+				k_close(pennfat_fd);
 				close(host_fd);
 			}
 			// cp SOURCE DEST (both in PennFAT)
 			else if (n_tokens == 3) {
 				// Open source file
-				int src_fd = k_open(&fs, tokens[1], F_READ);
+				int src_fd = k_open(tokens[1], F_READ);
 				if (src_fd < 0) {
 					fprintf(stderr, "cp: failed to open source file with error code %d\n", src_fd);
 					goto cleanup_tokens;
 				}
 				
 				// Create destination file
-				int dest_fd = k_open(&fs, tokens[2], F_WRITE);
+				int dest_fd = k_open(tokens[2], F_WRITE);
 				if (dest_fd < 0) {
 					fprintf(stderr, "cp: failed to create destination file with error code %d\n", dest_fd);
-					k_close(&fs, src_fd);
+					k_close(src_fd);
 					goto cleanup_tokens;
 				}
 				
 				// Copy data
 				char buffer[1024];
 				int bytes_read;
-				while ((bytes_read = k_read(&fs, src_fd, sizeof(buffer), buffer)) > 0) {
-					if (k_write(&fs, dest_fd, buffer, bytes_read) < 0) {
+				while ((bytes_read = k_read(src_fd, sizeof(buffer), buffer)) > 0) {
+					if (k_write(dest_fd, buffer, bytes_read) < 0) {
 						fprintf(stderr, "cp: failed to write to destination file\n");
-						k_close(&fs, src_fd);
-						k_close(&fs, dest_fd);
+						k_close(src_fd);
+						k_close(dest_fd);
 						goto cleanup_tokens;
 					}
 				}
 				
-				k_close(&fs, src_fd);
-				k_close(&fs, dest_fd);
+				k_close(src_fd);
+				k_close(dest_fd);
 			}
 			else {
 				fprintf(stderr, "cp: invalid arguments\n");
@@ -497,7 +497,7 @@ int main(void) {
 			uint8_t perm = tokens[1][0] - '0';
 			
 			// Open the file to update its permission
-			int fd = k_open(&fs, tokens[2], F_READ);
+			int fd = k_open(tokens[2], F_READ);
 			if (fd < 0) {
 				fprintf(stderr, "chmod: failed to open file with error code %d\n", fd);
 				goto cleanup_tokens;
@@ -508,161 +508,14 @@ int main(void) {
 			entry->ptr_to_dir_entry->perm = perm;
 			
 			// Write the updated directory entry back to disk
-			if (write_root_dir_entry(&fs, entry->ptr_to_dir_entry, 
+			if (write_root_dir_entry(entry->ptr_to_dir_entry, 
 				entry->dir_entry_block_num, entry->dir_entry_idx) != 0) {
 				fprintf(stderr, "chmod: failed to update file permissions\n");
-				k_close(&fs, fd);
+				k_close(fd);
 				goto cleanup_tokens;
 			}
 			
-			k_close(&fs, fd);
-			
-		} else if (strcmp(tokens[0], "ls") == 0) {
-			if (!is_mounted()) {
-				fprintf(stderr, "ls: there is no filesystem mounted\n");
-				goto cleanup_tokens;
-			}
-			
-			// cp -h HOST_FILE PENNFAT_FILE (from host to pennfat)
-			if (n_tokens == 4 && strcmp(tokens[1], "-h") == 0) {
-				// Open host file
-				int host_fd = open(tokens[2], O_RDONLY);
-				if (host_fd < 0) {
-					fprintf(stderr, "cp: failed to open host file: %s\n", tokens[2]);
-					goto cleanup_tokens;
-				}
-				
-				// Create destination file in PennFAT
-				int pennfat_fd = k_open(&fs, tokens[3], F_WRITE);
-				if (pennfat_fd < 0) {
-					fprintf(stderr, "cp: failed to create PennFAT file with error code %d\n", pennfat_fd);
-					close(host_fd);
-					goto cleanup_tokens;
-				}
-				
-				// Copy data
-				char buffer[1024];
-				ssize_t bytes_read;
-				while ((bytes_read = read(host_fd, buffer, sizeof(buffer))) > 0) {
-					if (k_write(&fs, pennfat_fd, buffer, bytes_read) < 0) {
-						fprintf(stderr, "cp: failed to write to PennFAT file\n");
-						close(host_fd);
-						k_close(&fs, pennfat_fd);
-						goto cleanup_tokens;
-					}
-				}
-				
-				close(host_fd);
-				k_close(&fs, pennfat_fd);
-			}
-			// cp PENNFAT_FILE -h HOST_FILE (from pennfat to host)
-			else if (n_tokens == 4 && strcmp(tokens[2], "-h") == 0) {
-				// Open PennFAT file
-				int pennfat_fd = k_open(&fs, tokens[1], F_READ);
-				if (pennfat_fd < 0) {
-					fprintf(stderr, "cp: failed to open PennFAT file with error code %d\n", pennfat_fd);
-					goto cleanup_tokens;
-				}
-				
-				// Create host file
-				int host_fd = open(tokens[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				if (host_fd < 0) {
-					fprintf(stderr, "cp: failed to create host file: %s\n", tokens[3]);
-					k_close(&fs, pennfat_fd);
-					goto cleanup_tokens;
-				}
-				
-				// Copy data
-				char buffer[1024];
-				int bytes_read;
-				while ((bytes_read = k_read(&fs, pennfat_fd, sizeof(buffer), buffer)) > 0) {
-					if (write(host_fd, buffer, bytes_read) < 0) {
-						fprintf(stderr, "cp: failed to write to host file\n");
-						k_close(&fs, pennfat_fd);
-						close(host_fd);
-						goto cleanup_tokens;
-					}
-				}
-				
-				k_close(&fs, pennfat_fd);
-				close(host_fd);
-			}
-			// cp SOURCE DEST (both in PennFAT)
-			else if (n_tokens == 3) {
-				// Open source file
-				int src_fd = k_open(&fs, tokens[1], F_READ);
-				if (src_fd < 0) {
-					fprintf(stderr, "cp: failed to open source file with error code %d\n", src_fd);
-					goto cleanup_tokens;
-				}
-				
-				// Create destination file
-				int dest_fd = k_open(&fs, tokens[2], F_WRITE);
-				if (dest_fd < 0) {
-					fprintf(stderr, "cp: failed to create destination file with error code %d\n", dest_fd);
-					k_close(&fs, src_fd);
-					goto cleanup_tokens;
-				}
-				
-				// Copy data
-				char buffer[1024];
-				int bytes_read;
-				while ((bytes_read = k_read(&fs, src_fd, sizeof(buffer), buffer)) > 0) {
-					if (k_write(&fs, dest_fd, buffer, bytes_read) < 0) {
-						fprintf(stderr, "cp: failed to write to destination file\n");
-						k_close(&fs, src_fd);
-						k_close(&fs, dest_fd);
-						goto cleanup_tokens;
-					}
-				}
-				
-				k_close(&fs, src_fd);
-				k_close(&fs, dest_fd);
-			}
-			else {
-				fprintf(stderr, "cp: invalid arguments\n");
-				goto cleanup_tokens;
-			}
-			
-		} else if (strcmp(tokens[0], "chmod") == 0) {
-			if (!is_mounted()) {
-				fprintf(stderr, "chmod: there is no filesystem mounted\n");
-				goto cleanup_tokens;
-			}
-			
-			if (n_tokens != 3) {
-				fprintf(stderr, "chmod: got wrong number of arguments\n");
-				goto cleanup_tokens;
-			}
-			
-			// Parse permission mode
-			if (strlen(tokens[1]) != 1 || tokens[1][0] < '0' || tokens[1][0] > '7') {
-				fprintf(stderr, "chmod: invalid permission mode (must be 0-7)\n");
-				goto cleanup_tokens;
-			}
-			
-			uint8_t perm = tokens[1][0] - '0';
-			
-			// Open the file to update its permission
-			int fd = k_open(&fs, tokens[2], F_READ);
-			if (fd < 0) {
-				fprintf(stderr, "chmod: failed to open file with error code %d\n", fd);
-				goto cleanup_tokens;
-			}
-			
-			// Update the permission (this requires direct access to the directory entry)
-			global_fd_entry *entry = &global_fd_table[fd];
-			entry->ptr_to_dir_entry->perm = perm;
-			
-			// Write the updated directory entry back to disk
-			if (write_root_dir_entry(&fs, entry->ptr_to_dir_entry, 
-				entry->dir_entry_block_num, entry->dir_entry_idx) != 0) {
-				fprintf(stderr, "chmod: failed to update file permissions\n");
-				k_close(&fs, fd);
-				goto cleanup_tokens;
-			}
-			
-			k_close(&fs, fd);
+			k_close(fd);
 			
 		} else if (strcmp(tokens[0], "ls") == 0) {
 			if (!is_mounted()) {
@@ -672,7 +525,7 @@ int main(void) {
 			
 			// If no arguments, list all files
 			if (n_tokens == 1) {
-				int ls_status = k_ls(&fs, NULL);
+				int ls_status = k_ls(NULL);
 				if (ls_status < 0) {
 					fprintf(stderr, "ls: failed with error code %d\n", ls_status);
 					goto cleanup_tokens;
@@ -680,7 +533,7 @@ int main(void) {
 			}
 			// List specific file
 			else if (n_tokens == 2) {
-				int ls_status = k_ls(&fs, tokens[1]);
+				int ls_status = k_ls(tokens[1]);
 				if (ls_status < 0) {
 					fprintf(stderr, "ls: failed to list file %s with error code %d\n", 
 						tokens[1], ls_status);
