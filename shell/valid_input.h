f3f5884a8bb48458a869f5dd86f820e242865ab6
luckyswaminathan <lakshman.swaminathan1@gmail.com>
2025-04-10 19:56:41 -0400

    refactor lib

--- shell/valid_input.h
+++
@@ -1,8 +0,0 @@
-#ifndef PENNSHELL_VALID_INPUT_H 
-#define PENNSHELL_VALID_INPUT_H
-
-#include "./parser.h"
-
-void validate_command(struct parsed_command *parsed_command);
-
-#endif // PENNSHELL_VALID_INPUT_H
