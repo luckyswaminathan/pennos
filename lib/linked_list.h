b5e7d956c7f966e39a328d2876a9a59324d35142
Arnav Chopra <arnav.chopra@gmail.com>
2025-04-14 23:35:21 -0400

    cleaned up linked list implementation

--- lib/linked_list.h
+++ lib/linked_list.h
@@ -1,135 +1,220 @@
-#include <assert.h>
-
-// Implementation of a doubly linked list.
-// It's heavily inspired by the vector.h
-// header from hw1.
-//
-// Usage:
-// typedef struct int_ll_node_st {
-//   int_ll_node_st* prev; // MUST be named prev
-//   int_ll_node_st* next; // MUST be named next
-//   int val;
-// } int_ll_node;
-// 
-// linked_list(int_ll_node) int_ll = linked_list_new(int_ll_node, NULL);
-// ...
-
-typedef void (*destroy_fn)(void*);
-
-/* Handle for the linked list which has the head and tail.
+/**
+ * @file linked_list.h
+ * @brief A generic doubly linked list implementation using macros
+ * 
+ * This header provides a generic doubly linked list implementation using C macros.
+ * It allows creating type-safe linked lists for any struct type.
+ * 
+ * Usage example:
+ * @code
+ * // Define a node structure
+ * typedef struct int_node_st {
+ *   struct int_node_st* prev;  // MUST be named prev
+ *   struct int_node_st* next;  // MUST be named next
+ *   int val;
+ * } int_node;
+ * 
+ * // Create a new linked list
+ * linked_list(int_node) int_list = {NULL, NULL, NULL};
+ * 
+ * // Add elements
+ * int_node* node = malloc(sizeof(int_node));
+ * node->val = 42;
+ * linked_list_push_tail(&int_list, node, prev, next);
+ * @endcode
  */
-// NOLINTNEXTLINE(bugprone-macro-parentheses)
-#define linked_list(T) struct { T * head; T * tail; destroy_fn ele_dtor; }
 
-#define linked_list_head(self) \
-  ({                           \
-    (self)->head;                 \
-  })
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
-
-#define linked_list_tail(self) \
-  ({                           \
-    (self)->tail;                 \
-  })
-
-#define linked_list_push_head(self, ele, prev_field, next_field) \
-  ({                                     \
-   if ((self)->head == NULL) {              \
-      (self)->head = ele;                   \
-      (self)->tail = ele;                   \
-      (ele)->prev_field = NULL;                  \
-      (ele)->next_field = NULL;                  \
-    } else {                             \
-      (self)->head->prev_field = ele;             \
-      (ele)->next_field = (self)->head;             \
-      (ele)->prev_field = NULL;                  \
-      (self)->head = ele;                   \
-    }                                    \
-  })
-
-
-#define linked_list_push_tail(self, ele, prev_field, next_field) \
-  ({                                     \
-   if ((self)->head == NULL) {              \
-      (self)->head = ele;                   \
-      (self)->tail = ele;                   \
-      (ele)->prev_field = NULL;                  \
-      (ele)->next_field = NULL;                  \
-    } else {                             \
-      (self)->tail->next_field = ele;             \
-      (ele)->prev_field = (self)->tail;             \
-      (ele)->next_field = NULL;                  \
-      (self)->tail = ele;                   \
-    }                                    \
-  })
+#include <assert.h>
+#include <stdlib.h>
 
 /**
- * Note: if ele does not belong to the list
- * then this is indeterminate
+ * @brief Function pointer type for element destructor
+ * 
+ * This function is called when an element is removed from the list
+ * and the list's ele_dtor field is set.
  */
-#define linked_list_remove(self, ele, prev_field, next_field) \
-  ({                             \
-    if ((ele) == NULL) { \
-      /* Do nothing */ \
+typedef void (*destroy_fn)(void*);
+
+/**
+ * @brief Macro to define a linked list structure for type T
+ * 
+ * Note: Type T must have 'prev' and 'next' pointer fields of type T*
+ */
+#define linked_list(T) struct { \
+    T* head;                    \
+    T* tail;                    \
+    destroy_fn ele_dtor;        \
+}
+
+/**
+ * @brief Initialize a new linked list
+ */
+#define linked_list_new(T, dtor) {NULL, NULL, (destroy_fn)(dtor)}
+
+/**
+ * @brief Get the head of the linked list
+ */
+#define linked_list_head(self) ((self)->head)
+
+/**
+ * @brief Get the tail of the linked list
+ */
+#define linked_list_tail(self) ((self)->tail)
+
+/**
+ * @brief Check if the linked list is empty
+ */
+#define linked_list_is_empty(self) ((self)->head == NULL)
+
+/**
+ * @brief Push an element to the head of the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_push_head(self, ele) \
+  do { \
+    if ((self)->head == NULL) { \
+      (self)->head = (ele); \
+      (self)->tail = (ele); \
+      (ele)->prev = NULL; \
+      (ele)->next = NULL; \
     } else { \
-      if ((ele)->next_field != NULL) { \
-        (ele)->next_field->prev_field = (ele)->prev_field; \
+      (self)->head->prev = (ele); \
+      (ele)->next = (self)->head; \
+      (ele)->prev = NULL; \
+      (self)->head = (ele); \
+    } \
+  } while (0)
+
+/**
+ * @brief Push an element to the tail of the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_push_tail(self, ele) \
+  do { \
+    if ((self)->head == NULL) { \
+      (self)->head = (ele); \
+      (self)->tail = (ele); \
+      (ele)->prev = NULL; \
+      (ele)->next = NULL; \
+    } else { \
+      (self)->tail->next = (ele); \
+      (ele)->prev = (self)->tail; \
+      (ele)->next = NULL; \
+      (self)->tail = (ele); \
+    } \
+  } while (0)
+
+/**
+ * @brief Remove an element from the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_remove(self, ele) \
+  do { \
+    if ((ele) != NULL) { \
+      if ((ele)->next != NULL) { \
+        (ele)->next->prev = (ele)->prev; \
         assert((self)->tail != (ele)); \
       } else { \
         assert((self)->tail == (ele)); \
-        (self)->tail = (ele)->prev_field; \
-      }\
-      if ((ele)->prev_field != NULL) { \
-        (ele)->prev_field->next_field = (ele)->next_field; \
+        (self)->tail = (ele)->prev; \
+      } \
+      if ((ele)->prev != NULL) { \
+        (ele)->prev->next = (ele)->next; \
         assert((self)->head != (ele)); \
       } else { \
         assert((self)->head == (ele)); \
-        (self)->head = (ele)->next_field; \
-     } \
-     (ele)->prev_field = NULL; \
-     (ele)->next_field = NULL; \
-   } \
+        (self)->head = (ele)->next; \
+      } \
+      (ele)->prev = NULL; \
+      (ele)->next = NULL; \
+    } \
+  } while (0)
+
+/**
+ * @brief Pop the head element from the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_pop_head(self) \
+  ({ \
+    typeof((self)->head) __ret = (self)->head; \
+    if (__ret != NULL) { \
+      linked_list_remove(self, __ret); \
+    } \
+    __ret; \
   })
 
-#define linked_list_pop_head(self, prev_field, next_field)  \
-  ({                                     \
-    typeof((self)->head) __ret = (self)->head;  \
-    linked_list_remove(self, (self)->head, prev_field, next_field); \
-    __ret;                               \
+/**
+ * @brief Pop the tail element from the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_pop_tail(self) \
+  ({ \
+    typeof((self)->tail) __ret = (self)->tail; \
+    if (__ret != NULL) { \
+      linked_list_remove(self, __ret); \
+    } \
+    __ret; \
   })
 
+/**
+ * @brief Get the next element in the list
+ * 
+ * @note The element type must have a 'next' pointer field
+ */
+#define linked_list_next(ele) ((ele)->next)
 
-#define linked_list_pop_tail(self, prev_field, next_field)       \
-  ({                                     \
-    typeof((self)->tail) __ret = (self)->tail;             \
-    linked_list_remove(self, (self)->tail, prev_field, next_field); \
-    __ret;                               \
-  })
+/**
+ * @brief Get the previous element in the list
+ * 
+ * @note The element type must have a 'prev' pointer field
+ */
+#define linked_list_prev(ele) ((ele)->prev)
 
-#define linked_list_next(ele, prev_field, next_field) \
-  ({                          \
-    (ele)->next_field;                \
-  })
-
-#define linked_list_prev(ele, prev_field, next_field) \
-  ({                          \
-    (ele)->prev_field;                \
-  })
-
-#define linked_list_clear(self, prev_field, next_field)          \
-  ({                                     \
+/**
+ * @brief Clear all elements from the list
+ * 
+ * @note The element type must have 'prev' and 'next' pointer fields
+ */
+#define linked_list_clear(self) \
+  do { \
     typeof((self)->head) __ele = (self)->head; \
-    typeof((self)->head) __next_ele;        \
-    (self)->head = NULL;                    \
-    (self)->tail = NULL;                    \
-    while (__ele != NULL) {              \
-      __next_ele = __ele->next_field;          \
-      __ele->prev_field = NULL;                \
-      __ele->next_field = NULL;                \
-      if ((self)->ele_dtor != NULL) {       \
-        (self)->ele_dtor(__ele);            \
-      }                                  \
-      __ele = __next_ele;                \
-    }                                    \
+    typeof((self)->head) __next_ele; \
+    (self)->head = NULL; \
+    (self)->tail = NULL; \
+    while (__ele != NULL) { \
+      __next_ele = __ele->next; \
+      __ele->prev = NULL; \
+      __ele->next = NULL; \
+      if ((self)->ele_dtor != NULL) { \
+        (self)->ele_dtor(__ele); \
+      } \
+      __ele = __next_ele; \
+    } \
+  } while (0)
+
+/**
+ * @brief Get the number of elements in the list
+ * 
+ * @note The element type must have a 'next' pointer field
+ */
+#define linked_list_size(self) \
+  ({ \
+    size_t __count = 0; \
+    typeof((self)->head) __ele = (self)->head; \
+    while (__ele != NULL) { \
+      __count++; \
+      __ele = __ele->next; \
+    } \
+    __count; \
   })
+
+#endif /* LINKED_LIST_H */
 
---
+++ lib/tests/test_linked_list.c
@@ -0,0 +1,192 @@
+/**
+ * @file test_linked_list.c
+ * @brief Unit tests for the linked_list.h implementation
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "../linked_list.h"
+
+/* Test node structure for integers */
+typedef struct int_node_st {
+    struct int_node_st* prev;  // MUST be named prev
+    struct int_node_st* next;  // MUST be named next
+    int val;
+} int_node;
+
+/* Test node structure for strings */
+typedef struct str_node_st {
+    struct str_node_st* prev;  // MUST be named prev
+    struct str_node_st* next;  // MUST be named next
+    char* str;
+} str_node;
+
+/* Function to free string nodes */
+void free_str_node(void* node) {
+    str_node* n = (str_node*)node;
+    free(n->str);
+    free(n);
+}
+
+/* Function to free integer nodes */
+void free_int_node(void* node) {
+    free(node);
+}
+
+/* Helper function to create an integer node */
+int_node* create_int_node(int val) {
+    int_node* node = malloc(sizeof(int_node));
+    node->prev = NULL;
+    node->next = NULL;
+    node->val = val;
+    return node;
+}
+
+/* Helper function to create a string node */
+str_node* create_str_node(const char* str) {
+    str_node* node = malloc(sizeof(str_node));
+    node->prev = NULL;
+    node->next = NULL;
+    node->str = strdup(str);
+    return node;
+}
+
+/* Test function to verify list operations */
+void test_int_list() {
+    printf("Testing integer linked list...\n");
+    
+    /* Create a new list */
+    linked_list(int_node) int_list = linked_list_new(int_node, free_int_node);
+    assert(linked_list_is_empty(&int_list));
+    assert(linked_list_size(&int_list) == 0);
+    
+    /* Add elements to the list */
+    int_node* node1 = create_int_node(1);
+    int_node* node2 = create_int_node(2);
+    int_node* node3 = create_int_node(3);
+    
+    linked_list_push_tail(&int_list, node1);
+    assert(!linked_list_is_empty(&int_list));
+    assert(linked_list_size(&int_list) == 1);
+    assert(linked_list_head(&int_list) == node1);
+    assert(linked_list_tail(&int_list) == node1);
+    
+    linked_list_push_tail(&int_list, node2);
+    assert(linked_list_size(&int_list) == 2);
+    assert(linked_list_head(&int_list) == node1);
+    assert(linked_list_tail(&int_list) == node2);
+    assert(linked_list_next(node1) == node2);
+    assert(linked_list_prev(node2) == node1);
+    
+    linked_list_push_head(&int_list, node3);
+    assert(linked_list_size(&int_list) == 3);
+    assert(linked_list_head(&int_list) == node3);
+    assert(linked_list_tail(&int_list) == node2);
+    assert(linked_list_next(node3) == node1);
+    assert(linked_list_prev(node1) == node3);
+    
+    /* Remove elements from the list */
+    int_node* popped_head = linked_list_pop_head(&int_list);
+    assert(popped_head == node3);
+    assert(linked_list_size(&int_list) == 2);
+    assert(linked_list_head(&int_list) == node1);
+    
+    int_node* popped_tail = linked_list_pop_tail(&int_list);
+    assert(popped_tail == node2);
+    assert(linked_list_size(&int_list) == 1);
+    assert(linked_list_tail(&int_list) == node1);
+    
+    /* Clear the list */
+    linked_list_clear(&int_list);
+    assert(linked_list_is_empty(&int_list));
+    assert(linked_list_size(&int_list) == 0);
+    
+    /* Free the popped nodes */
+    free(popped_head);
+    free(popped_tail);
+    
+    printf("Integer linked list tests passed!\n");
+}
+
+/* Test function to verify string list operations */
+void test_str_list() {
+    printf("Testing string linked list...\n");
+    
+    /* Create a new list */
+    linked_list(str_node) str_list = linked_list_new(str_node, free_str_node);
+    assert(linked_list_is_empty(&str_list));
+    
+    /* Add elements to the list */
+    str_node* node1 = create_str_node("Hello");
+    str_node* node2 = create_str_node("World");
+    str_node* node3 = create_str_node("!");
+    
+    linked_list_push_tail(&str_list, node1);
+    linked_list_push_tail(&str_list, node2);
+    linked_list_push_head(&str_list, node3);
+    
+    assert(linked_list_size(&str_list) == 3);
+    assert(strcmp(linked_list_head(&str_list)->str, "!") == 0);
+    assert(strcmp(linked_list_tail(&str_list)->str, "World") == 0);
+    
+    /* Remove a node from the middle */
+    str_node* middle = linked_list_next(linked_list_head(&str_list));
+    linked_list_remove(&str_list, middle);
+    assert(linked_list_size(&str_list) == 2);
+    assert(strcmp(linked_list_head(&str_list)->str, "!") == 0);
+    assert(strcmp(linked_list_tail(&str_list)->str, "World") == 0);
+    assert(linked_list_next(linked_list_head(&str_list)) == linked_list_tail(&str_list));
+    
+    /* Free the removed node */
+    free_str_node(middle);
+    
+    /* Clear the list */
+    linked_list_clear(&str_list);
+    assert(linked_list_is_empty(&str_list));
+    
+    printf("String linked list tests passed!\n");
+}
+
+/* Test function to verify edge cases */
+void test_edge_cases() {
+    printf("Testing edge cases...\n");
+    
+    /* Create a new list */
+    linked_list(int_node) list = linked_list_new(int_node, free_int_node);
+    
+    /* Test operations on empty list */
+    assert(linked_list_pop_head(&list) == NULL);
+    assert(linked_list_pop_tail(&list) == NULL);
+    
+    /* Test with a single element */
+    int_node* node = create_int_node(42);
+    linked_list_push_head(&list, node);
+    assert(linked_list_size(&list) == 1);
+    assert(linked_list_head(&list) == node);
+    assert(linked_list_tail(&list) == node);
+    
+    /* Remove the only element */
+    linked_list_remove(&list, node);
+    assert(linked_list_is_empty(&list));
+    free(node);
+    
+    printf("Edge case tests passed!\n");
+}
+
+int main() {
+    printf("Running linked list tests...\n\n");
+    
+    test_int_list();
+    printf("\n");
+    
+    test_str_list();
+    printf("\n");
+    
+    test_edge_cases();
+    printf("\n");
+    
+    printf("All tests passed!\n");
+    return 0;
+} 
