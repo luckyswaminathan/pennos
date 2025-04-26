f86b2b35461298a6e8e72dfb2a4a291c31abf153
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2025-04-25 22:13:25 -0400

    Implement touch, cp, rm, chmod

--- src/shell/commands.c
+++ src/shell/commands.c
@@ -9,6 +9,7 @@
 #include "../scheduler/sys.h"
 #include "../../lib/exiting_alloc.h"
 #include "../scheduler/spthread.h"
+#include "src/pennfat/fat_constants.h"
 
 #define BUFFER_SIZE 256
 #define BUSY_LOOP_ITERATIONS 100000000
@@ -170,6 +171,8 @@ void* nice_command(void* arg) {
     return NULL;
 }
 
+// TODO: make sure we s_exit() in every single case for all commands
+
 void* ls(void* arg) {
     struct command_context* ctx = (struct command_context*)arg;
     s_ls(ctx->command[1]); // will be NULL if no argument is provided
@@ -181,6 +184,171 @@ void* echo(void* arg) {
     char** command = (char**) arg;
     int ret = s_write(STDOUT_FILENO, command[1], strlen(command[1]));
     s_exit(ret);
+    return NULL;
+}
+
+void* touch(void* arg) {
+    char** command = (char**)arg;
+    if (command[1] == NULL)
+    {
+        char* error_message = "touch got wrong number of arguments (expected at least 1 argument)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        return NULL;
+    }
+    for (int i = 1; command[i] != NULL; i++) {
+        int fd = s_open(command[i], F_APPEND);
+        if (fd < 0)
+        {
+            char* error_message = "touch: failed to open file\n";
+            s_write(STDERR_FILENO, error_message, strlen(error_message));
+            s_exit(fd);
+            return NULL;
+        }
+        int status = s_write(fd, NULL, 0);
+        if (status < 0)
+        {
+            char* error_message = "touch: failed to write to file\n";
+            s_write(STDERR_FILENO, error_message, strlen(error_message));
+            s_exit(status);
+            return NULL;
+        }
+        s_close(fd);
+    }
+    s_exit(0);
+    return NULL;
+}
+
+void* rm(void* arg) {
+    char** command = (char**)arg;
+    if (command[1] == NULL)
+    {
+        char* error_message = "rm got wrong number of arguments (expected at least 1 argument)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        return NULL;
+    }
+
+    for (int i = 1; command[i] != NULL; i++) {
+        int unlink_status = s_unlink(command[i]);
+        if (unlink_status < 0)
+        {
+            char* error_message = "rm: Error - failed to remove file\n";
+            s_write(STDERR_FILENO, error_message, strlen(error_message));
+            s_exit(unlink_status);
+            return NULL;
+        }
+    }
+    s_exit(0);
+    return NULL;
+}
+
+void* cp(void* arg) {
+    char** command = (char**)arg;
+
+    bool has_three_args = command[1] != NULL && command[2] != NULL && command[3] == NULL;
+    if (!has_three_args)
+    {
+        char* error_message = "cp got wrong number of arguments (expected at  2 arguments)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        return NULL;
+    }
+
+    // Open source file
+    int src_fd = s_open(command[1], F_READ);
+    if (src_fd < 0)
+    {
+        char* error_message = "cp: Error - failed to open source file\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_exit(src_fd);
+        return NULL;
+    }
+
+    // Open destination file (overwrite, or create if it doesn't exist)
+    int dest_fd = s_open(command[2], F_WRITE);
+    if (dest_fd < 0)
+    {
+        char* error_message = "cp: Error - failed to create destination file\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_close(src_fd);
+        s_exit(dest_fd);
+        return NULL;
+    }
+
+    // Copy data
+    char buffer[1024];
+    int bytes_read;
+    while ((bytes_read = s_read(src_fd, sizeof(buffer), buffer)) > 0)
+    {
+        if (s_write(dest_fd, buffer, bytes_read) < 0)
+        {
+            char* error_message = "cp: Error - failed to write to destination file\n";
+            s_write(STDERR_FILENO, error_message, strlen(error_message));
+            s_close(src_fd);
+            s_close(dest_fd);
+            s_exit(dest_fd);
+            return NULL;
+        }
+    }
+
+    s_close(src_fd);
+    s_close(dest_fd);
+    s_exit(0);
+    return NULL;
+}
+
+void* chmod(void* arg) {
+    char** command = (char**)arg;
+    bool has_two_args = command[1] != NULL && command[2] != NULL && command[3] == NULL;
+    if (!has_two_args)
+    {
+        char* error_message = "chmod got wrong number of arguments (expected 2 arguments)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_exit(-200);
+        return NULL;
+    }
+
+    // Parse permission mode
+    char* permission_mode = command[1];
+    if (strlen(permission_mode) < 2 || strlen(permission_mode) > 4)
+    {
+        char* error_message = "chmod: Error - invalid permission mode (must be 2-4 characters long including + or -)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_exit(-201);
+        return NULL;
+    }
+
+    int mode;
+    if (permission_mode[0] == '+') {
+        mode = F_CHMOD_ADD;
+    } else if (permission_mode[0] == '-') {
+        mode = F_CHMOD_REMOVE;
+    } else {
+        char* error_message = "chmod: Error - invalid permission mode (must start with + or -)\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_exit(-202);
+        return NULL;
+    }
+
+    // NOTE: we allow for repeats of the same type of permission (e.g +rr or -www or +xxr)
+    uint8_t permissions = 0;
+    for (int i = 1; i < strlen(permission_mode); i++) {
+        if (permission_mode[i] == 'r') {
+            permissions |= F_CHMOD_R;
+        } else if (permission_mode[i] == 'w') {
+            permissions |= F_CHMOD_W;
+        } else if (permission_mode[i] == 'x') {
+            permissions |= F_CHMOD_X;
+        }
+    }
+
+    int chmod_status = s_chmod(command[2], permissions, mode);
+    if (chmod_status < 0)
+    {
+        char* error_message = "chmod: Error - failed to change permissions\n";
+        s_write(STDERR_FILENO, error_message, strlen(error_message));
+        s_exit(chmod_status);
+        return NULL;
+    }
+    s_exit(0);
     return NULL;
 }
 
@@ -207,6 +375,18 @@ void* execute_command(void* arg) {
     }
     if (strcmp(ctx[0], "echo") == 0) {
         return echo(ctx);
+    }
+    if (strcmp(ctx[0], "touch") == 0) {
+        return touch(ctx);
+    }
+    if (strcmp(ctx[0], "cp") == 0) {
+        return cp(ctx);
+    }
+    if (strcmp(ctx[0], "rm") == 0) {
+        return rm(ctx);
+    }
+    if (strcmp(ctx[0], "chmod") == 0) {
+        return chmod(ctx);
     }
     s_exit(0);
     return NULL;
