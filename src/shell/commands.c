01120859a0527ee1998de952db0f3ec8ad3da86c
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2025-04-26 00:00:56 -0400

    Actually add cat

--- src/shell/commands.c
+++ src/shell/commands.c
@@ -535,6 +535,9 @@ void* execute_command(void* arg) {
     if (strcmp(ctx[0], "chmod") == 0) {
         return chmod(ctx);
     }
+    if (strcmp(ctx[0], "cat") == 0) {
+        return cat(ctx);
+    }
     if (strcmp(ctx[0], "busy") == 0) {
         return busy(ctx, ctx[1]);
     }
